2378b0236c0f307d7e0f5f81805e23627e6e1ac4
weldon-araujo <weldonaraujo100@gmail.com>
2024-01-13 20:01:32 -0300

    more update

--- main.py
+++ main.py
@@ -8,33 +8,28 @@ from args import options
 arguments = options()
 
 
-
 def status_200():
-    pass
-
-if arguments.target:
-    target =arguments.target
     with open(arguments.wordlist, 'r') as fil:
         lines = fil.readlines()
         for line in lines:
-            req = requests.get(target+line)
+            req = requests.get(instance_target+line)
             if req.status_code == 200:
-                print(f'success {"200"} {line}')
+                print(f'success {req.status_code} {line}')
+
+
+def main_status():         
+    with open(arguments.wordlist, 'r') as fil:
+        lines = fil.readlines()
+        for line in lines:
+            req = requests.get(instance_target+line)
+            if req.status_code == 200:
+                print(f'success {req.status_code} {line}')
             elif req.status_code == 301:
                 print(f'redirect {req.status_code} {line}')
             elif req.status_code == 403:
                 print(f'forbidden {req.status_code} {line}')
-  
 
 
-
-# aqui é o teste de colorir a saida que ainda vou implementar depois
-'''
-colorama.init()
-
-print(colorama.Fore.RED + "Esta string é vermelha!")
-print(colorama.Fore.GREEN + "Esta string é verde!")
-print(colorama.Fore.YELLOW + "Esta string é amarela!")
-
-colorama.deinit()
-'''
+if arguments.target:
+    instance_target = arguments.target
+    main_status()
