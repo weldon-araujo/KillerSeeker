cc20f795dac3e62e388967a238b76645fa0e454b
weldon-araujo <weldonaraujo100@gmail.com>
2024-01-13 20:02:30 -0300

    new update

--- args.py
+++ args.py
@@ -6,7 +6,7 @@ def options():
     parser.add_argument('-u', dest='target', required=True, help='target for spider')
     parser.add_argument('-w', dest='wordlist', required=True, help='file or path wordlist')
     parser.add_argument('-T', dest='time', required=None, help='time for every single request')
+    parser.add_argument('-c', dest='status', required=None, help='only chose request')
 
     return parser.parse_args()
 
-#options()
