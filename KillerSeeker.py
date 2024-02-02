d0474862fd2c2c382cb5f1305466a2ad34d7fc10
weldon-araujo <weldonaraujo100@gmail.com>
2024-02-02 07:57:50 -0300

    update

--- KillerSeeker.py
+++ KillerSeeker.py
@@ -25,8 +25,13 @@ def main_status():
     with open(arguments.wordlist, 'r') as fil:
         lines = fil.readlines()
         for line in lines:
-            url = urljoin("https://" + instance_target + "/", line)
-            req = requests.get(url)
+            url = urljoin("http://" + instance_target + "/", line)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'}
+            if arguments.useragent:
+                headers = headers.copy()
+            else:
+                headers = {}
+            req = requests.get(url, headers=headers)
             if req.status_code == 200:
                 print(f'{instance_target}/{line} {colored(req.status_code, "green")}')
             elif req.status_code == 301:
@@ -34,13 +39,10 @@ def main_status():
             elif req.status_code == 302:
                 print(f'{instance_target}/{line} {colored(req.status_code, "blue")}')
             elif req.status_code == 403:
-                print(f'{instance_target}/{line}{colored(req.status_code, "yellow")}')
-
+                print(f'{instance_target} {line} {colored(req.status_code, "yellow")}\t' * (10))
             
-def useragent():
-    pass
 
-
+    
 if arguments.target:
     instance_target = arguments.target
     main_status()
